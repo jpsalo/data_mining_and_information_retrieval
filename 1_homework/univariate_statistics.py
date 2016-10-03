859ea23afb0bc7b1a883d4100825173dc4091af3
jp <jpsalo@zoho.com>
2016-10-03 13:52:39 +0200

    Initial commit. Draw a bar chart of the frequencies of the number of comments from a sample of 100 items.

---
+++ 1_homework/univariate_statistics.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('ggplot')
+
+posts = pd.read_csv('~/Dropbox/koulu/data_mining_and_information_retrieval/HN_posts_year_to_Sep_26_2016.csv')
+
+posts_sample = posts.sample(n = 100)
+posts_comments = posts_sample["num_comments"].value_counts().sort_index()
+
+plt.figure()
+
+plot = posts_comments.plot.bar(title = 'frequency')
+plot.set_xlabel('comments')
+plot.set_ylabel('amount')
+
+# http://stackoverflow.com/a/25163682
+plt.show()
