c05f8027ba971276b9135f435b9bf9b3ceda517c
jp <jpsalo@zoho.com>
2016-10-04 21:30:26 +0200

    Fixed histogram that displays number of comments to use number of comments instead of frequencies of the number of comments.

--- 1_homework/univariate_statistics.py
+++ 1_homework/univariate_statistics.py
@@ -6,7 +6,7 @@ import matplotlib
 matplotlib.style.use('ggplot')
 
 posts = pd.read_csv('~/Dropbox/koulu/data_mining_and_information_retrieval/HN_posts_year_to_Sep_26_2016.csv')
-posts_sample = posts.sample(n = 100)
+posts_sample = posts.sample(n=100)
 
 # NOMINAL, ORDINAL
 
@@ -24,10 +24,10 @@ titles = posts['title']
 
 title_lengths = list(map(lambda x: len(x), titles))
 
-df = pd.Series(title_lengths).value_counts().sort_index()
+title_lengths_df = pd.Series(title_lengths).value_counts().sort_index()
 
 plt.figure(1)
-plot = df.plot.bar(title ='Lengths of the titles')
+plot = title_lengths_df.plot.bar(title='Lengths of the titles')
 plot.set_xlabel('Length (characters)')
 plot.set_ylabel('Frequency')
 
@@ -37,10 +37,10 @@ plot.set_ylabel('Frequency')
 
 # Number of comments the posts received
 
-comments = posts['num_comments'].value_counts().sort_index()
+comments = posts['num_comments']
 
 plt.figure(2)
-plot = comments.plot.hist(title = 'Number of comments the posts received', bins=20)
+plot = comments.plot.hist(title='Number of comments the posts received', bins=20)
 plot.set_xlabel('Comments')
 plot.set_ylabel('Frequency')
 
