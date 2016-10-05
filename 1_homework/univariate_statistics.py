881320921a54066bab9d0a90414b33eee164662a
jp <jpsalo@zoho.com>
2016-10-05 12:35:32 +0200

    Added location statistics. Refactored existing code into functions.

--- 1_homework/univariate_statistics.py
+++ 1_homework/univariate_statistics.py
@@ -18,39 +18,66 @@ posts_sample = posts.sample(n=100)
 
 # ORDINAL
 
-# Lengths of the blog post titles
+def generate_frequency():
+    # Lengths of the blog post titles
+    titles = posts['title']
 
-titles = posts['title']
+    title_lengths = list(map(lambda x: len(x), titles))
 
-title_lengths = list(map(lambda x: len(x), titles))
+    title_lengths_df = pd.Series(title_lengths).value_counts().sort_index()
 
-title_lengths_df = pd.Series(title_lengths).value_counts().sort_index()
+    plt.figure(1)
+    plot = title_lengths_df.plot.bar(title='Lengths of the titles')
+    plot.set_xlabel('Length (characters)')
+    plot.set_ylabel('Frequency')
 
-plt.figure(1)
-plot = title_lengths_df.plot.bar(title='Lengths of the titles')
-plot.set_xlabel('Length (characters)')
-plot.set_ylabel('Frequency')
+generate_frequency()
 
 # QUANTITATIVE
 
 # Distributions
+def generate_histogram():
+    # Number of comments the posts received
+    comments = posts['num_comments']
 
-# Number of comments the posts received
+    plt.figure(2)
+    plot = comments.plot.hist(title='Number of comments the posts received', bins=20)
+    plot.set_xlabel('Comments')
+    plot.set_ylabel('Frequency')
 
-comments = posts['num_comments']
+generate_histogram()
 
-plt.figure(2)
-plot = comments.plot.hist(title='Number of comments the posts received', bins=20)
-plot.set_xlabel('Comments')
-plot.set_ylabel('Frequency')
 
 # Standard deviation
+def generate_standard_deviation():
+    comments = posts['num_comments']
+    means = comments.mean()
+    errors = comments.std()
+    plt.figure(3)
+    fig, ax = plt.subplots()
+    pd.Series(means).plot.bar(yerr=errors, ax=ax)
 
-means = comments.mean()
-errors = comments.std()
-plt.figure(3)
-fig, ax = plt.subplots()
-pd.Series(means).plot.bar(yerr=errors, ax=ax)
+generate_standard_deviation()
+
+
+# Mean absolute deviation
+def calculate_mean_absolute_deviation():
+    comments = posts['num_comments']
+    pd.Series(comments).mad()
+
+calculate_mean_absolute_deviation()
+
+
+# Location
+def generate_box_plot(data, attribute):
+    values_df = pd.DataFrame(data[attribute])
+
+    plt.figure(4)
+    values_df.plot.box(showfliers=False)
+    plt.figure(5)
+    values_df.plot.box()
+
+generate_box_plot(posts, 'num_points')
 
 # http://stackoverflow.com/a/25163682
 plt.show()
