68270378f626cbbcca5f6a098ecc39845b524da0
jp <jpsalo@zoho.com>
2016-10-05 23:56:11 +0200

    Added descriptive title and labels to the figures. Use logarithmic scale where appropriate. Save figures into files.

--- 2_homework/univariate_statistics.py
+++ 2_homework/univariate_statistics.py
@@ -2,11 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from os.path import expanduser
+
+# http://stackoverflow.com/a/4028943
+home = expanduser("~")
 
 matplotlib.style.use('ggplot')
 
-posts = pd.read_csv('~/Dropbox/koulu/data_mining_and_information_retrieval/HN_posts_year_to_Sep_26_2016.csv')
-posts_sample = posts.sample(n=100)
+BASE_PATH = home + '/Dropbox/koulu/data_mining_and_information_retrieval/'
+FIGURE_PATH = BASE_PATH + 'figures/'
+
+posts = pd.read_csv(BASE_PATH + 'HN_posts_year_to_Sep_26_2016.csv')
 
 # NOMINAL, ORDINAL
 
@@ -19,19 +25,20 @@ posts_sample = posts.sample(n=100)
 # ORDINAL
 
 
-def generate_frequency(values, title, x_label):
+def generate_frequency(values, title, x_label, name_suffix):
     values_df = pd.Series(values).value_counts().sort_index()
 
     plt.figure(1)
     plot = values_df.plot.bar(title=title)
     plot.set_xlabel(x_label)
     plot.set_ylabel('Frequency')
+    plt.savefig(FIGURE_PATH + 'frequency_' + name_suffix + '.png')
 
 # QUANTITATIVE
 
 
 # Distributions
-def generate_histogram(data, attribute, title, x_label):
+def generate_histogram(data, attribute, title, x_label, name_suffix):
     # Number of comments the posts received
     values = data[attribute]
 
@@ -39,16 +46,18 @@ def generate_histogram(data, attribute, title, x_label):
     # http://stackoverflow.com/a/21033789
     plot = values.plot.hist(title=title, bins=20, bottom=0.1, logy=True)
     plot.set_xlabel(x_label)
-    plot.set_ylabel('Frequency')
+    plot.set_ylabel('Frequency (log)')
+    plt.savefig(FIGURE_PATH + 'histogram_' + name_suffix + '.png')
 
 
 # Standard deviation
-def generate_standard_deviation(data, attribute):
+def generate_standard_deviation(data, attribute, title, name_suffix):
     values = data[attribute]
     means = values.mean()
     errors = values.std()
     fig, ax = plt.subplots()
-    pd.Series(means).plot.bar(yerr=errors, ax=ax)
+    pd.Series(means).plot.bar(title=title, yerr=errors, ax=ax, logy=True)
+    plt.savefig(FIGURE_PATH + 'standard_deviation_' + name_suffix + '.png')
 
 
 # Mean absolute deviation
@@ -57,13 +66,14 @@ def calculate_mean_absolute_deviation(data, attribute):
 
 
 # Location
-def generate_box_plot(data, attribute):
+def generate_box_plot(data, attribute, x_label, name_suffix, hide_fliers=False):
     values_df = pd.DataFrame(data[attribute])
+    title = 'Quartiles (without outliers)' if hide_fliers else 'Quartiles'
 
-    plt.figure(3)
-    values_df.plot.box(showfliers=False)
-    plt.figure(4)
-    values_df.plot.box()
+    plot = values_df.plot.box(title=title, showfliers=not hide_fliers, logy=True)
+    plt.xticks([1], [x_label])
+    plot.set_ylabel('log')
+    plt.savefig(FIGURE_PATH + 'box_plot_' + name_suffix + '.png')
 
 
 def calculate_lengths(data, attribute):
@@ -75,11 +85,13 @@ def calculate_lengths(data, attribute):
 # Lengths of the blog post titles
 posts_title_lengths = calculate_lengths(posts, 'title')
 
-generate_frequency(posts_title_lengths, 'Lengths of the titles', 'Length (characters)')
-generate_histogram(posts, 'num_comments', 'Number of comments the posts received', 'Comments')
-generate_standard_deviation(posts, 'num_comments')
+generate_frequency(posts_title_lengths, 'Lengths of the titles', 'Length (characters)', 'title_lengths')
+generate_histogram(posts, 'num_comments', 'Number of comments the posts received', 'Comments', 'comments')
 calculate_mean_absolute_deviation(posts, 'num_comments')
-generate_box_plot(posts, 'num_points')
+generate_standard_deviation(posts, 'num_comments', 'Standard deviation of the number of comments', 'comments')
+generate_box_plot(posts, 'num_points', 'Comments', 'comments')
+generate_box_plot(posts, 'num_points', 'Comments', 'comments_without_outliers',  True)
 
+# For presentation and debug
 # http://stackoverflow.com/a/25163682
-plt.show()
+# plt.show()
