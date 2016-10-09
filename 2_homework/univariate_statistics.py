d418fbcdb18ad67fb92c4c8681d7bb732745081c
jp <jpsalo@zoho.com>
2016-10-09 21:03:37 +0200

    Added common function to save figures into a file. Also, read blog posts train data CSV file.

--- 2_homework/univariate_statistics.py
+++ 2_homework/univariate_statistics.py
@@ -9,10 +9,13 @@ home = expanduser("~")
 
 matplotlib.style.use('ggplot')
 
+DEBUG_MODE = False
+
 BASE_PATH = home + '/Dropbox/koulu/data_mining_and_information_retrieval/'
 FIGURE_PATH = BASE_PATH + 'figures/'
 
 posts = pd.read_csv(BASE_PATH + 'HN_posts_year_to_Sep_26_2016.csv')
+blogs = pd.read_csv(BASE_PATH + 'BlogFeedback/blogData_train.csv', header=None)
 
 # NOMINAL, ORDINAL
 
@@ -25,39 +28,40 @@ posts = pd.read_csv(BASE_PATH + 'HN_posts_year_to_Sep_26_2016.csv')
 # ORDINAL
 
 
-def generate_frequency(values, title, x_label, name_suffix):
+def generate_frequency(values, title, x_label):
     values_df = pd.Series(values).value_counts().sort_index()
 
-    plt.figure(1)
+    fig = plt.figure()
     plot = values_df.plot.bar(title=title)
     plot.set_xlabel(x_label)
     plot.set_ylabel('Frequency')
-    plt.savefig(FIGURE_PATH + 'frequency_' + name_suffix + '.png')
+    return fig
+
 
 # QUANTITATIVE
 
 
 # Distributions
-def generate_histogram(data, attribute, title, x_label, name_suffix):
+def generate_histogram(data, attribute, title, x_label):
     # Number of comments the posts received
     values = data[attribute]
 
-    plt.figure(2)
+    fig = plt.figure()
     # http://stackoverflow.com/a/21033789
     plot = values.plot.hist(title=title, bins=20, bottom=0.1, logy=True)
     plot.set_xlabel(x_label)
     plot.set_ylabel('Frequency (log)')
-    plt.savefig(FIGURE_PATH + 'histogram_' + name_suffix + '.png')
+    return fig
 
 
 # Standard deviation
-def generate_standard_deviation(data, attribute, title, name_suffix):
+def generate_standard_deviation(data, attribute, title):
     values = data[attribute]
     means = values.mean()
     errors = values.std()
     fig, ax = plt.subplots()
     pd.Series(means).plot.bar(title=title, yerr=errors, ax=ax, logy=True)
-    plt.savefig(FIGURE_PATH + 'standard_deviation_' + name_suffix + '.png')
+    return fig
 
 
 # Mean absolute deviation
@@ -66,14 +70,19 @@ def calculate_mean_absolute_deviation(data, attribute):
 
 
 # Location
-def generate_box_plot(data, attribute, x_label, name_suffix, hide_fliers=False):
+def generate_box_plot(data, attribute, x_label, hide_fliers=False):
     values_df = pd.DataFrame(data[attribute])
     title = 'Quartiles (without outliers)' if hide_fliers else 'Quartiles'
 
-    plot = values_df.plot.box(title=title, showfliers=not hide_fliers, logy=True)
+    fig = plt.figure()
+
+    plot = values_df.boxplot(return_type='axes', showfliers=not hide_fliers)
+    plot.set_yscale('log')
     plt.xticks([1], [x_label])
+
+    plot.set_title(title)
     plot.set_ylabel('log')
-    plt.savefig(FIGURE_PATH + 'box_plot_' + name_suffix + '.png')
+    return fig
 
 
 def calculate_lengths(data, attribute):
@@ -82,16 +91,32 @@ def calculate_lengths(data, attribute):
     return values_lengths
 
 
+def process_plot(figure, type, name_suffix):
+    if (not DEBUG_MODE):
+        plt.savefig(FIGURE_PATH + type + '_' + name_suffix + '.png')
+
+
 # Lengths of the blog post titles
 posts_title_lengths = calculate_lengths(posts, 'title')
 
-generate_frequency(posts_title_lengths, 'Lengths of the titles', 'Length (characters)', 'title_lengths')
-generate_histogram(posts, 'num_comments', 'Number of comments the posts received', 'Comments', 'comments')
+frequency_figure = generate_frequency(posts_title_lengths, 'Lengths of the titles', 'Length (characters)')
+process_plot(frequency_figure, 'frequency', 'title_lengths')
+#
+histogram_figure = generate_histogram(posts, 'num_comments', 'Number of comments the posts received', 'Comments')
+process_plot(histogram_figure, 'histogram', 'comments')
+
 calculate_mean_absolute_deviation(posts, 'num_comments')
-generate_standard_deviation(posts, 'num_comments', 'Standard deviation of the number of comments', 'comments')
-generate_box_plot(posts, 'num_points', 'Comments', 'comments')
-generate_box_plot(posts, 'num_points', 'Comments', 'comments_without_outliers',  True)
+
+standard_deviation_figure = generate_standard_deviation(posts, 'num_comments',
+                                                        'Standard deviation of the number of comments')
+process_plot(standard_deviation_figure, 'standard_deviation', 'comments')
+
+box_plot_figure = generate_box_plot(posts, 'num_points', 'Comments')
+process_plot(box_plot_figure, 'box_plot', 'comments')
+
+box_plot_without_outliers_figure = generate_box_plot(posts, 'num_points', 'Comments',  True)
+process_plot(box_plot_without_outliers_figure, 'box_plot', 'comments_without_outliers')
 
 # For presentation and debug
 # http://stackoverflow.com/a/25163682
-# plt.show()
+plt.show()
