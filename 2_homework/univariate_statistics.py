6bc2ee0bae47222b4f65d5ba8d41a1df64cd8b01
jp <jpsalo@zoho.com>
2016-10-13 17:11:07 +0200

    Added support for scatter plot and time series graph.

--- 2_homework/univariate_statistics.py
+++ 2_homework/univariate_statistics.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from datetime import datetime
 from os.path import expanduser
 
-# http://stackoverflow.com/a/4028943
-home = expanduser("~")
-
+home = expanduser("~")  # http://stackoverflow.com/a/4028943
+# Suppress SettingWithCopyWarning
+# http://stackoverflow.com/a/20627316
+pd.options.mode.chained_assignment = None
 matplotlib.style.use('ggplot')
-
 DEBUG_MODE = False
 
 BASE_PATH = home + '/Dropbox/koulu/data_mining_and_information_retrieval/'
@@ -101,6 +102,28 @@ def generate_box_plot(data, attribute, data_set_name, x_label, hide_fliers=False
     return fig
 
 
+def generate_scatter_plot(data, x_attribute, y_attribute, data_set_name, title, x_label, y_label):
+    title = title + '\n' + data_set_name
+    plot = data.plot.scatter(x=x_attribute, y=y_attribute)
+    plot.set_title(title)
+    plot.set_ylabel(y_label)
+    plot.set_xlabel(x_label)
+    return plot
+
+
+# http://stackoverflow.com/a/13674286
+# https://blog.mafr.de/2012/03/11/time-series-data-with-matplotlib/
+# http://stackoverflow.com/a/17004818
+def generate_time_series(data, x_attribute, y_attribute, data_set_name, title, x_label, y_label):
+    title = title + '\n' + data_set_name
+    fig, ax = plt.subplots()
+    ax.plot_date(x=data[x_attribute], y=data[y_attribute])
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    return fig
+
+
 def calculate_lengths(data, attribute):
     values = data[attribute]
     values_lengths = list(map(lambda x: len(x), values))
@@ -115,7 +138,14 @@ def generate_bins(values):
 
 
 def get_weekday_frequencies(values):
+    # http://stackoverflow.com/a/11287278
     return blogs.ix[:,269:275].sum()
+
+
+def parse_time(value):
+    # http://stackoverflow.com/a/16139085
+    value_datetime = datetime.strptime(value, "%m/%d/%Y %H:%M")
+    return value_datetime.time()
 
 
 def process_plot(figure, type, data_set_name, name_suffix):
@@ -172,6 +202,16 @@ process_plot(box_plot_figure, 'box_plot', get_data_set_name('blogs'), 'comments'
 
 box_plot_without_outliers_figure = generate_box_plot(blogs, 50, get_data_set_name('blogs'), 'Comments', True)
 process_plot(box_plot_without_outliers_figure, 'box_plot', get_data_set_name('blogs'), 'comments_without_outliers')
+
+scatter_plot = generate_scatter_plot(posts, 'num_comments', 'num_points', get_data_set_name('posts'),
+                                     'Correlation between comments and points', 'Comments', 'Points')
+process_plot(scatter_plot, 'scatter_plot', get_data_set_name('posts'), 'comments_points')
+
+posts_slice = posts[['num_comments', 'created_at']]
+posts_slice['created_at_time'] = posts_slice['created_at'].apply(parse_time)
+time_series_figure = generate_time_series(posts_slice, 'created_at_time', 'num_comments', get_data_set_name('posts'),
+                                          'Comments by hour of submission', 'Hour of submission', 'Comments')
+process_plot(time_series_figure, 'time_series', get_data_set_name('posts'), 'comments')
 
 # For presentation and debug
 # http://stackoverflow.com/a/25163682
